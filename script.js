8bc25ccf3a10bd793897b8abac631733c11406f2
Kirama97 <officialthiam97@gmail.com>
2025-01-21 17:40:24 +0000

    insta parametre

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ burguer.onclick = function (){
     nav.classList.toggle("active")
     
 }
+
+
+
+const bt_parametre = document.querySelectorAll('.bt_parametre');
+const bloc_parametre = document.querySelectorAll('.bloc_parametre');
+
+
+bt_parametre.forEach((element , index) => {
+     element.onclick = ()=> {
+      
+         element.classList.toggle("toucher");
+         
+         if (bloc_parametre[index]) {
+            bloc_parametre[index].classList.toggle("active");
+        }
+
+
+         
+     } 
+});
